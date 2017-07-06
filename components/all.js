d0dcf55b56c9eb70e02d3335eb858b96e06d97d1
Ellie <ellie@magnon.net>
2017-07-06 15:32:50 +0200

    Reexport magnon-progress-bar

--- components/all.js
+++ components/all.js
@@ -9,6 +9,7 @@ export { MagnonImage } from "./magnon-image/magnon-image.js";
 export { MagnonLogo } from "./magnon-logo/magnon-logo.js";
 export { MagnonNotifications } from "./magnon-notifications/magnon-notifications.js";
 export { MagnonNotification } from "./magnon-notifications/magnon-notification.js";
+export { MagnonProgressBar } from "./magnon-progress-bar/magnon-progress-bar.js";
 export { MagnonShell } from "./magnon-shell/magnon-shell.js";
 export { MagnonSocialButton, createSocialIconButton } from "./magnon-social-buttons/magnon-social-buttons.js";
 export { MagnonSpinner } from "./magnon-spinner/magnon-spinner.js";
